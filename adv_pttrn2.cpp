2c4eda925423805c5cd0692a0779feaf00cb5926
NIRUJOGI NANDAN <97619905+nnandan21@users.noreply.github.com>
2023-09-02 16:12:31 +0530

    Update adv_pttrn2.cpp

--- adv_pttrn2.cpp
+++ adv_pttrn2.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 using namespace std;
+
 int main(){
-    int n=5;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=i;j++){
-            if((i+j)%2==0){
-                cout<<"1 ";
+    int n;
+    cin >> n; // Input the value of 'n'
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= i; j++){
+            // Check if the sum of 'i' and 'j' is even
+            if((i + j) % 2 == 0){
+                cout << "1 "; // If even, print '1'
             }
             else{
-                cout<<"0 ";
+                cout << "0 "; // If odd, print '0'
             }
         }
-        cout<<endl;
+        cout << endl; // Move to the next line after each row
     }
-    return 0;
+
+    return 0; // Return 0 to indicate successful execution
 }
