2b8b59318a2ad013e1b26989eeae43fcaa763514
NIRUJOGI NANDAN <97619905+nnandan21@users.noreply.github.com>
2023-09-02 16:06:48 +0530

    Update adv_pttrn4.cpp

--- adv_pttrn4.cpp
+++ adv_pttrn4.cpp
@@ -1,14 +1,24 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n=5;
-    for(int i=1;i<=n;i++){
-        for(int j=n-i;j>=1;j--){
-            cout<<" ";
+
+int main() {
+    int n;
+    cin >> n; // Read the value of 'n' from the user
+
+    // Outer loop to iterate through each row
+    for (int i = 1; i <= n; i++) {
+        // Inner loop to print spaces before the numbers
+        for (int j = n - i; j >= 1; j--) {
+            cout << " "; // Print a space for alignment
         }
-        for(int j=1;j<=i;j++){
-            cout<<j<<" ";
+        
+        // Inner loop to print numbers in ascending order
+        for (int j = 1; j <= i; j++) {
+            cout << j << " "; // Print the current number followed by a space
         }
-    cout<<endl;
+
+        cout << endl; // Move to the next line after each row is printed
     }
+
+    return 0; // Exit the program
 }
