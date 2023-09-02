7ce43aacf73cac5760e65a37d7c2436dddc96d1d
NIRUJOGI NANDAN <97619905+nnandan21@users.noreply.github.com>
2023-09-02 16:07:31 +0530

    Update adv_pttrn3.cpp

--- adv_pttrn3.cpp
+++ adv_pttrn3.cpp
@@ -1,13 +1,22 @@
- #include <iostream>
+#include <iostream>
 using namespace std;
-int main(){
-    int n=5;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<"  ";
+
+int main() {
+    int n;
+    cin >> n; // Read the value of 'n' from the user
+
+    // Outer loop to iterate through each row
+    for (int i = 1; i <= n; i++) {
+        // Inner loop to print spaces before the asterisks
+        for (int j = 1; j <= n - i; j++) {
+            cout << "  "; // Print two spaces for alignment
         }
-        cout<<"* * * * *";
-        cout<<endl;
+        
+        // Print a row of five asterisks
+        cout << "* * * * *";
+        
+        cout << endl; // Move to the next line after each row is printed
     }
-    return 0;
+
+    return 0; // Exit the program
 }
